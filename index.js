03f3c1359322bb020373078764eb74db40ef7dd7
GnipN <nattapon.nr@gmail.com>
2025-04-06 23:49:34 +0700

    webhook example

--- index.js
+++ index.js
@@ -1,21 +1,93 @@
 const express = require('express');
 const axios = require('axios');
+const https = require('https');
 const app = express();
 const port = 3000;
 
 app.use(express.json());
-
-// Line messaging API credentials
-const LINE_USER_ID = "U4..."; 
-const LINE_ACCESS_TOKEN = "qF...";
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
 
 // Test endpoint
 app.get('/', (req, res) => {
   res.send('Hello, World!');
 });
 
+// LINE webhook endpoint
+app.post("/webhook", function (req, res) {
+  console.log("Received webhook data:", req.body);
+  console.log("Received webhook source:", req.body.events[0].source);
+  res.send("HTTP POST request sent to the webhook URL!");
+  // If the user sends a message to your bot, send a reply message
+  if (req.body.events[0].type === "message") {
+    // You must stringify reply token and message data to send to the API server
+    const dataString = JSON.stringify({
+      // Define reply token
+      replyToken: req.body.events[0].replyToken,
+      // Define reply messages
+      messages: [
+        {
+          type: "text",
+          text: "Hello, user",
+        },
+        {
+          type: "text",
+          text: "May I help you?",
+        },
+      ],
+    });
+
+    // Request header. See Messaging API reference for specification
+    const headers = {
+      "Content-Type": "application/json",
+      Authorization: "Bearer " + LINE_ACCESS_TOKEN,
+    };
+
+    // Options to pass into the request, as defined in the http.request method in the Node.js documentation
+    const webhookOptions = {
+      hostname: "api.line.me",
+      path: "/v2/bot/message/reply",
+      method: "POST",
+      headers: headers,
+      body: dataString,
+    };
+
+    // When an HTTP POST request of message type is sent to the /webhook endpoint,
+    // we send an HTTP POST request to https://api.line.me/v2/bot/message/reply
+    // that is defined in the webhookOptions variable.
+
+    // Define our request
+    const request = https.request(webhookOptions, (res) => {
+      res.on("data", (d) => {
+        process.stdout.write(d);
+      });
+    });
+
+    // Handle error
+    // request.on() is a function that is called back if an error occurs
+    // while sending a request to the API server.
+    request.on("error", (err) => {
+      console.error(err);
+    });
+
+    // Finally send the request and the data we defined
+    // request.write(dataString);
+    request.end();
+  }
+});
+
+// Endpoint to send a message to LINE
 app.post('/send-line-message', async (req, res) => {
-  const { message } = req.body;
+  // Example request body:
+  // {
+  //   "message": "test message to LINE",
+  //   "LINE_USER_ID": "C8xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+  //   "LINE_ACCESS_TOKEN": "qFxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+  // }
+  const { message, LINE_USER_ID, LINE_ACCESS_TOKEN } = req.body;
   try {
     const response = await axios.post(
       'https://api.line.me/v2/bot/message/push',
